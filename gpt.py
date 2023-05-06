6b26977e2747a42fc1f38ac5a6879372b79da4c7
Jiwook Chung <jwc@Jiwooks-MBP.attlocal.net>
2023-05-06 16:54:10 -0700

    first fine tuning partially successful

--- gpt.py
+++ gpt.py
@@ -1,13 +1,13 @@
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 model_engine = "text-davinci-003"
 s = input("type here: ")
 
 completion = openai.Completion.create(
-        engine = model_engine,
+        model = "curie:ft-personal-2023-05-06-23-03-22",
         prompt = str(s),
         max_tokens = 1024,
         n=1,
